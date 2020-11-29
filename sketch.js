805e03ec7e121d99ad61df44c164ae6f9357a868
subhavasu <70517919+subhavasu@users.noreply.github.com>
2020-11-29 16:42:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,142 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+var score=0;
+var block1,block2,block3,block4,block5,block6,block7,block8,block9,block10;
+var block11,block12,block13,block14,block15,block16;
+var b1,b2,b3,b4,b5,b6,b7,b8,b9;
+var blocks1,blocks2,blocks3,blocks4,blocks5,blocks6,blocks7,blocks8,blocks9,blocks10;
+var blocks11,blocks12,blocks13,blocks14,blocks15,blocks16;
+
+
+function setup() {
+  createCanvas(1200,600);
+  
+  engine = Engine.create();
+  world = engine.world;
+
+  Engine.run(engine)
+
+  ground = new Ground(600,580,1200,20);
+
+  ground2 = new Ground(390,300,260,10);
+  ground3 = new Ground(900,400,200,10);
+
+  polygon = new Polygon(150,200,30,30);
+
+  launcher = new Slingshot(polygon.body,{x:150, y:110})
+  
+  
+  block1 = new Block(480,275,30,40);
+  block2 = new Block(450,275,30,40);
+  block3 = new Block(420,275,30,40);
+  block4 = new Block(390,275,30,40);
+  block5 = new Block(360,275,30,40);
+  block6 = new Block(330,275,30,40);
+  block7 = new Block(300,275,30,40);
+  block8 = new Block(330,235,30,40);
+  block9 = new Block(360,235,30,40);
+  block10 = new Block(390,235,30,40);
+  block11 = new Block(420,235,30,40);
+  block12 = new Block(450,235,30,40);
+  block13 = new Block(360,195,30,40);
+  block14 = new Block(390,195,30,40);
+  block15 = new Block(420,195,30,40);
+  block16 = new Block(390,155,30,40);
+
+  b1 = new Block(900,275,30,40);
+  b2 = new Block(900,315,30,40);
+  b3 = new Block(870,315,30,40);
+  b4 = new Block(930,315,30,40);
+  b5 = new Block(900,355,30,40);
+  b6 = new Block(870,355,30,40);
+  b7 = new Block(840,355,30,40);
+  b8 = new Block(930,355,30,40);
+  b9 = new Block(960,355,30,40);
+  
+
+
+
+
+}
+
+function draw() {
+  background(255);
+  textSize(35)
+  fill("RED")
+  text("SCORE: "+score,600,100);
+
+  ground.display();
+  ground2.display();
+  ground3.display();
+
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  block13.display();
+  block14.display();
+  block15.display();
+  block16.display(); 
+
+  b1.display();
+  b2.display();
+  b3.display();
+  b4.display();
+  b5.display();
+  b6.display();
+  b7.display();
+  b8.display();
+  b9.display();
+
+  polygon.display();
+
+  launcher.display();
+  block1.score();
+  block2.score();
+  block3.score();
+  block4.score();
+  block5.score();
+  block6.score();
+  block7.score();
+  block8.score();
+  block9.score();
+  block10.score();
+  block11.score();
+  block12.score();
+  block13.score();
+  block14.score();
+  block15.score();
+  block16.score();
+
+  drawSprites();
+}
+
+function mouseDragged()  {
+  Matter.Body.setPosition(polygon.body,{x: mouseX, y: mouseY})
+}
+
+function mouseReleased()  {
+  launcher.fly()
+  
+
+}
+
+function keyPressed()  {
+  if(keyCode===32)  {
+    Matter.Body.setPosition(polygon.body,{x:150, y:200})
+    launcher.attach(polygon.body);
+
+  }
+
+}
